19a172ee1ee81fdb7e1ca9cd1371747b1e9502eb
Abdullah Alsuhaimy <a.alsuhaimy@labayh.net>
2023-08-01 15:10:08 +0300

    added all

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task =   {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  }; 
+  faTimes = faTimes
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
